5ee11c33cee558be9759ad5b6595f7a74c54eb33
Adam Malec <add.malec@gmail.com>
2025-06-22 12:03:56 +0300

    feat: add [email] ROUTE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import '../app.css';
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<h1 class="text-3xl text-center">Welcome to PokeTeam</h1>
+<h1 class="text-4xl text-center">Welcome to PokeTeam</h1>
 <p class="text-balance text-center text-lg">Create an account to make your own pokemon team with your own unique URL</p>
 <Button href="/login" class="p-6 text-base uppercase">Create an account</Button>
---
+++ src/routes/[email]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import Card from '@/components/ui/card.svelte';
+
+  let pageName = $derived(page.params.email);
+</script>
+
+
+<h1 class="text-3xl">{pageName}'s Page</h1>
+<ul class="flex gap-6">
+  <li><Card></Card></li>
+  <li><Card></Card></li>
+  <li><Card></Card></li>
+</ul>
